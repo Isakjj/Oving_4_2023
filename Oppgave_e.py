f292b5324f0a46ac44e4eb226876546b7504a489
Marhoxo <145400851+Marhoxo@users.noreply.github.com>
2023-09-26 23:50:27 +0200

    Byttet språk og laget Oppg j
    Byttet språk i oppgave e til engelsk.
    Laget oppgave j

--- Oppgave_e.py
+++ Oppgave_e.py
@@ -5,30 +5,24 @@ Created on Sun Sep 24 00:06:44 2023
 @author: Mhodne
 """
 
-#Skriv en funksjon som tar inn ei liste med tall. For hvert tall i lista unntatt det siste skal
-#funksjonen regne ut differansen mellom neste tall i lista og dette tallet. Differansene skal
-#legges inn i ei ny liste.
-
-
-listname = [-8, -5, -4, -2, 1, 3, 7, 9, 11, 17]
-
 def diff(listname):
-    #Lager en ny liste utenfor for loopen.
+    #Makes a new list outside the loop
     newlist = []
     i = 0
-    #For loopen kjører for så mange elementer som er i listen 
+    #The loop runs for as many elements in the list.
     for element in range(1, len(listname)):
-            #Lager en ny variabel som er differansen mellom et element og elementet under.
+            #Makes a new variabel thats the difference between one element and the element below in the list.
             diff = int(listname[i+1]) - int(listname[i])
-            #Legger den nye variabelen inn i den nye listen.
+            #Puts the new element into the new list.
             newlist.append(diff)
-            #Legger til en slik at regnestykket flytter seg et element opp listen.
+            #Adds 1 so that calculation moves one step up the list.
             i += 1
-    #Returnerer den nye listen
+    #Return the new list.
     return newlist
 
-
-#Lager en variabel som blir satt lik den nye listen.       
-difflist = diff(listname)
-print(difflist)
+if __name__ == "__main__":
+    listname = [1, 4, 6, 7, 9, 13, 14, 22, 24, 25]
+#Makes a new variable and sets gives it the value og the new list.      
+    difflist = diff(listname)
+    print(difflist)
        
---
+++ Oppgave_j.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 23:30:18 2023
+
+@author: Mhodne
+"""
+
+from lister_for_del_1 import temperaturer
+from Oppgave_e import diff
+
+temp_up_down = diff(temperaturer)
+
+for i in temp_up_down:
+    if temp_up_down[i] < 0:
+        print(i)
+        print("The temperature is decreasing.")
+    elif temp_up_down[i] > 0:
+        print(i)
+        print("The temperature is rising.")
+    else:
+        print(i)
+        print("The temperature is unchanged.")
